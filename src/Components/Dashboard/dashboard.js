d0466428fead8b1faf83363c201760c731c22f4f
lucas hellanio <lucas.hellanio@liferay.com>
2021-10-04 22:33:23 -0300

    master | dashboard

---
+++ src/Components/Dashboard/dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import HeaderGithub from '../Header/header-github';
+import CardListGithub from '../Cardlist/card-list-github'
+
+const Dashboard = (props) => {
+    return (
+        <div  >
+    <HeaderGithub />
+    <CardListGithub />
+        </div>
+    );
+};
+
+export default Dashboard
+
